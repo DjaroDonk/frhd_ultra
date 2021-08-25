1808eb0b1c87e7e0a8e98be76ae74cfc85b30f5a
Djaro Donk <41857903+DjaroDonk@users.noreply.github.com>
2021-08-25 23:04:21 +0200

    accidentally broke it, then fixed it
    didn't add anything, just spent a lot of time doing useless stuff

--- procedural_gen.py
+++ procedural_gen.py
@@ -21,18 +21,16 @@ if track_long.lower() == "yes":
     letter_font.type_string(my_track,-40,-100,"This Track is long")
 pos = [[-40,100],[200,100]]
 segments = int(input("How many segments?\n"))
-#xdev = [int(i) for i in input("What is the range of x length ('min,max')\n").split(",")]
-exec("def xdev(i):\n\treturn("+ input("What is the range of x length ('min,max')\n")+")")
-exec("def ydev(i):\n\treturn("+ input("What is the range of y length ('min,max')\n")+")")
-#ydev = [int(i) for i in input("What is the range of y length ('min,max')\n").split(",")]
+xdev = [int(i) for i in input("What is the range of x length ('min,max')\n").split(",")]
+ydev = [int(i) for i in input("What is the range of y length ('min,max')\n").split(",")]
 checkdev = int(input("How many segments should there be between checkpoints?\n"))
 my_track.insLine(pos[0][0],pos[0][1],pos[1][0],pos[1][1])
 letter_font.type_string(my_track,pos[1][0]+50,pos[1][1]-50,"0/"+str(segments//checkdev),'s')
 my_track.insCheck(pos[1][0]+20,pos[1][1]-30)
 for i in range(1,segments+1):
     pos[0] = pos[1]
-    #pos[1] = [pos[1][0] + random.randint(xdev[0],xdev[1]),pos[1][1] + random.randint(ydev[0],ydev[1])]
-    pos[1] = [pos[1][0]+xdev(i),pos[1][1]+ydev(i)]
+    pos[1] = [pos[1][0] + random.randint(xdev[0],xdev[1]),pos[1][1] + random.randint(ydev[0],ydev[1])]
+    #pos[1] = [pos[1][0]+xdev(i),pos[1][1]+ydev(i)]
     xlen = pos[1][0] - pos[0][0]
     ylen = pos[1][1] - pos[0][1]
     my_track.insLine(pos[0][0],pos[0][1],pos[1][0],pos[1][1])
